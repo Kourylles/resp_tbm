61c5fa5a01b44cd5345429bbf1267c52e005ef94
kourylles <sitemarchand@gmail.com>
2023-07-12 09:20:22 +0200

    About component rendu responsive pour portable

--- components/about/aboutSite.vue
+++ components/about/aboutSite.vue
@@ -12,13 +12,13 @@
     <div class="bg-slate-900 text-slate-400 text-center m-4 h-full p-2">
       <p class="text-2xl m-4">{{ aboutStore.dataAbout.develloppeur }}</p>
       <img class="h-20 w-20 mx-auto mt-4 mb-20"  src="/images/accueil/GWF.gif" alt="Logo GWF">
-      <p class="text-2xl my-10">{{ aboutStore.dataAbout.juriPhotoSite1 }}</p>
-      <p class="text-2xl -m-10">{{ aboutStore.dataAbout.juriPhotoSite2 }}</p>
-      <p class="border-slate-400 border h-auto w-56 rounded mt-36 text-center mx-auto text-white"> Version 2.2.4.3 
+      <p class="text-2xl md:my-10">{{ aboutStore.dataAbout.juriPhotoSite1 }}</p>
+      <p class="text-2xl mt-4 md:-m-10">{{ aboutStore.dataAbout.juriPhotoSite2 }}</p>
+      <p class="border-slate-400 mt-14 border h-auto w-56 rounded md:mt-36 text-center mx-auto text-white"> Version 2.2.4.3 
           <br> Dernière mise à jour du site 
           <br>le 12/05/2023
         </p>
-      <button class="text-slate-400 p-2 h-auto w-28 mt-32 bg-gradient-to-br from-slate-700 to-slate-500 rounded-lg hover:bg-gradient-to-br hover:from-slate-500 hover:to-slate-300 hover:text-slate-900 font-bold md:text-xl" @click="open = false">Fermer</button>
+      <button class="text-slate-400 mt-28 mb-4 p-2 h-auto w-28 md:mt-32 bg-gradient-to-br from-slate-700 to-slate-500 rounded-lg hover:bg-gradient-to-br hover:from-slate-500 hover:to-slate-300 hover:text-slate-900 font-bold md:text-xl" @click="open = false">Fermer</button>
     </div>
   </div>
 </Teleport>
