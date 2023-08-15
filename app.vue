e431f4e2415ce8c6008d046a5abb03dd07798370
kourylles <sitemarchand@gmail.com>
2023-08-15 18:18:17 +0200

    Ajout de la transition entre les pages

--- app.vue
+++ app.vue
@@ -5,3 +5,17 @@
 		</NuxtLayout>
 	</div>
 </template>
+
+<style>
+.page-enter-from {
+	opacity: 0;
+	transform: translateX(200px);
+}
+.page-enter-active {
+	transition: all 0.9s;
+}
+.page-leave-to {
+	opacity: 0;
+	transform: translateX(-200px);
+}
+</style>
